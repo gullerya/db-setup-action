d239b08d17cf56dce3e2a42811e9c966f63bd817
Yuri Guller <gullerya@gmail.com>
2021-04-11 14:16:26 +0300

    fix

--- bin/utils.js
+++ bin/utils.js
@@ -52,7 +52,7 @@ function _execDocker(params, { captureOutput = false, reflectOutput = true } = {
 		console.info('=== spawning ' + '='.repeat(51));
 		console.info(`docker ${params.join(' ')}`);
 		console.info('='.repeat(64));
-		const child = spawn('docker', params);
+		const child = spawn('docker', params, { windowsVerbatimArguments: true });
 
 		child.stderr.on('data', errorCollector);
 		if (captureOutput || reflectOutput) {
