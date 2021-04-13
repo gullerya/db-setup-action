ee401fcb237dc57875bd87dc896ec50a13cfd56a
Yuri Guller <gullerya@gmail.com>
2021-04-13 20:54:44 +0300

    issue #1: setup of mysql

--- bin/mysql/mysql-setup.js
+++ bin/mysql/mysql-setup.js
@@ -60,8 +60,11 @@ async function healthCheck(cname, setup) {
 	const isDbAvailable = await retryUntil(
 		`Assert DB '${setup.database}' available`,
 		async () => {
+			// const status = await dockerExec([
+			// 	`${cname} mysql -u${setup.username} -p${setup.password} -e "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='${setup.database}'"`
+			// ]);
 			const status = await dockerExec([
-				`${cname} mysql -u${setup.username} -p${setup.password} -e "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='${setup.database}'"`
+				`${cname} less var/run/mysqld/mysqld.sock`
 			]);
 			return status.trim() === '1'
 		},
